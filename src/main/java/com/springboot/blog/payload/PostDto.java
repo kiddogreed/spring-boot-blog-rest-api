6bcec76ee1b433f12359677f43791d37ee2e1265
john russelle <kiddogreed@gmail.com>
2022-08-31 00:16:02 +0800

    create post controller model repo

---
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -0,0 +1,12 @@
+package com.springboot.blog.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDto  {
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+
+}
